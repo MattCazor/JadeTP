a07afb8b4d483dd621c9290c086f5fd1603c5c0a
Kelyan Pegeot Selme <kelyan.pegeot-selme@eviden.com>
2024-11-20 12:11:19 +0100

    feat(supabase-auth): Add dependencies and login redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,25 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react"
+
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    handleOnPageLoad();
+  }, []);
+
+  const handleOnPageLoad = () => {
+    // chekc if user is logged in
+    const jwt = localStorage.getItem('jwt');
+    if (jwt) {
+      // TO DO
+      // check first jwt is still valid
+    } else {
+      // redirect to login
+      router.push('/login');
+    }
+  };
+
   return (
     <div className="flex flex-col">
       <h1 className="text-white">Hello GrafBase</h1>
